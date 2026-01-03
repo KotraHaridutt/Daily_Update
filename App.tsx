0313f38fc04d4ce8ef44294fa240db59a68b4037
KOTRA HARIDUTT <139229443+KotraHaridutt@users.noreply.github.com>
2026-01-03 17:40:58 +0000

    del:index.css file

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './index.css'
+
 import { LedgerEntry, DailyStats } from './types';
 import { LedgerService, getTodayISO } from './services/ledgerService';
 import { DailyEntryForm } from './components/DailyEntryForm';
